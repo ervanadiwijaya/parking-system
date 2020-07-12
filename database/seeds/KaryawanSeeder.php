dddfbf9ddc1aeb74897fc4bb0ebc3224fed5b8ea
Kevin Adam <rexprimeadam@gmail.com>
2020-07-12 23:19:34 +0700

    add karyawan seeder

---
+++ database/seeds/KaryawanSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class KaryawanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'      => 'Mimin',
+            'email'     => 'admin@admin.com',
+            'password'  => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+    }
+}
