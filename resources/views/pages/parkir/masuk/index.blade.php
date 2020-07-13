454f5c4e44d71bcd3f41ec22ee3972b1187f52e3
Kevin Adam <rexprimeadam@gmail.com>
2020-07-14 00:17:26 +0700

    remove prefix in parkir

--- resources/views/pages/parkir/masuk/index.blade.php
+++ resources/views/pages/parkir/masuk/index.blade.php
@@ -17,10 +17,6 @@
                         <input name="prefix" value="" type="text" class="form-control">
                     </div>
                     <div class="form-group">
-                        <label for="exampleInputEmail1">Prefix</label>
-                        <input name="name" value="" type="text" class="form-control">
-                    </div>
-                    <div class="form-group">
                         <label for="exampleInputEmail1">No Polisi</label>
                         <input name="no_polisi" type="number" class="form-control">
                     </div>
