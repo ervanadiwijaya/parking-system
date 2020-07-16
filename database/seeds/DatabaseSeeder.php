3efaf853e080fd2f96f576beb41588ca89630297
Kevin Adam <rexprimeadam@gmail.com>
2020-07-16 12:09:37 +0700

    update dummy data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,14 +14,21 @@ class DatabaseSeeder extends Seeder
         $this->call(KaryawanSeeder::class);
         $this->call(JenisKendaraanSeeder::class);
         $this->call(UpahSeeder::class);
-        $parkir = factory('App\Parkir', 212)->create()->each(function($parkir){
-            if ($parkir->status) {
-                $parkir->transaksi()->save(
-                    factory('App\Transaksi')->make([
-                        'parkir_id'     => $parkir->id
-                    ])
-                );
-            }
-        });
+
+        for ($i=1; $i <= date('m'); $i++) { 
+            $parkir = factory('App\Parkir', rand(10, 300))->create([
+                'created_at' => date('Y-m-d H:i:s', strtotime('2020-'.$i.'-'.date('d').' 06:30:10'))
+            ])->each(function($parkir) use($i){
+                if ($parkir->status) {
+                    $parkir->transaksi()->save(
+                        factory('App\Transaksi')->make([
+                            'parkir_id'     => $parkir->id,
+                            'created_at'    => date('Y-m-d H:i:s', strtotime('2020-'.$i.'-'.date('d').' 17:30:10'))
+                        ])
+                    );
+                }
+            });
+        }
+
     }
 }
